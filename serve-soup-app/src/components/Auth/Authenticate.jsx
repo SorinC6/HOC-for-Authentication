aa1f2f7791514a9d27c3c2021f040ac400663406
Sorin Chis <kish.sorin@yahoo.com>
2019-04-15 19:38:30 +0300

    added HOC component for Autheticatication

---
+++ serve-soup-app/src/components/Auth/Authenticate.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import axios from "axios";
+
+const Authenticate = App => LoginPage => {
+  return class extends React.Component {
+    state = {
+      showError: false,
+      loggedIn: false,
+      token: null
+    };
+
+    componentDidMount() {
+      if (localStorage.getItem("token")) {
+        this.setState({
+          loggedIn: true,
+          token: localStorage.getItem("token")
+        });
+      } else {
+        this.setState({ loggedIn: false });
+        localStorage.clear();
+      }
+    }
+
+    render() {
+      if (this.state.loggedIn) {
+        return <App />;
+      } else {
+        return <Login />;
+      }
+    }
+  };
+};
+
+export default Authenticate;
